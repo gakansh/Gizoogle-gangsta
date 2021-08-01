7ef89e5ec504e033a71a7d954608b298fc84d5f2
gakansh_ <akg686023@gmail.com>
2021-08-01 22:23:44 +0530

    GizoogleAdded

--- bot.py
+++ bot.py
@@ -1,5 +1,24 @@
 import requests
 import json
+import re
+import bs4
+from urllib import parse
+import argparse
+
+
+def text(input_text: str) -> str:
+    params = {"translatetext": input_text}
+    target_url = "http://www.gizoogle.net/textilizer.php"
+    resp = requests.post(target_url, data=params)
+    # the html returned is in poor form normally.
+    soup_input = re.sub(
+        "/name=translatetext[^>]*>/", 'name="translatetext" >', resp.text)
+    soup = bs4.BeautifulSoup(soup_input, "lxml")
+    giz = soup.find_all(text=True)
+    giz_text = giz[37].strip("\r\n")  # Hacky, but consistent.
+    print(giz_text)
+    return giz_text
+
 
 
 
@@ -24,6 +43,11 @@ def make_reply():
     reply = "Okazz"
     return reply
 
+def translate(msg):
+    gurl = "http://www.gizoogle.net/textilizer.php"
+    
+
+
 
 update_id = None
 while True:
@@ -38,5 +62,5 @@ while True:
             except:
                 msg = None
             frm = item["message"]["from"]["id"]
-            reply = make_reply()
-            sendmessage(msg, frm)
+            reply = text(msg)
+            sendmessage(reply, frm)
